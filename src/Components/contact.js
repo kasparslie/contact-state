4768e86e92f934fde25e83c2d5e9e58e635a0144
Kaspars Liepins <kasparslie@gmail.com>
2020-10-13 22:09:34 +0200

    innitial commit

---
+++ src/Components/contact.js
@@ -0,0 +1,23 @@
+import React, {Component} from  'react'
+import './style.css'
+
+
+
+class Contact extends Component {
+    render () {
+      return (
+        <div className="Contact">
+            <img src="https://i.ibb.co/VqNQKWj/lizars.jpg" className="avatar" alt="lizard"></img>
+            <div>
+            <div className="name">John Dick
+            </div>
+            <div className="status">
+            <div className="status-online"></div>
+            <p className="status-text">Online</p>
+            </div>
+            </div>
+        </div>
+      )
+    } 
+    }
+export default Contact
