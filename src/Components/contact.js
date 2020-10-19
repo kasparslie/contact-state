e9c12c914eb3f852cbb046ae2d44e2b38304ac3b
Kaspars Liepins <kasparslie@gmail.com>
2020-10-19 12:25:01 +0200

    added states

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,19 +1,25 @@
 import React, {Component} from  'react'
 import './style.css'
 
-
 class Contact extends Component {
+  constructor(props) {
+      super(props);
+      this.state ={Online:false}
+  }
+  chStatus =()=> {
+    const status = !this.state.Online;
+    this.setState({Online:status}) ;}
     render () {
-      const {fullname, status, image}=this.props;
+      const {fullname, image}=this.props;
       return (
         <div className="Contact">
             <img src={image} className="avatar"></img>
             <div>
             <div className="name">{fullname}
             </div>
-            <div className="status">
-            <div className={status? "status-online" : "status-offline"}></div>
-            <div className="status-text">{status? 'online' : 'offline'}</div>
+            <div className="status" onClick={this.chStatus}>
+            <div className={this.state.Online? "status-online" : "status-offline"}></div>
+            <div className="status-text">{this.state.Online? 'online' : 'offline'}</div>
             </div>
             </div>
         </div>
